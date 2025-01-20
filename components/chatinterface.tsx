77cc8d5ed2bda6fff74a821583e891dabc61c38d
seyeonJeong <wjdtpdus828@naver.com>
2025-01-20 05:18:37 +0000

    change

--- components/chatinterface.tsx
+++ components/chatinterface.tsx
@@ -29,7 +29,7 @@ export default function ChatInterface() {
     async (userMessage: string): Promise<string> => {
       try {
         const res = await fetch(
-          'https://072f-165-132-192-52.ngrok-free.app/api/chat',
+          'https://c51f-165-132-192-52.ngrok-free.app/api/chat',
           {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
